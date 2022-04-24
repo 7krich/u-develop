54e7994776b4936b381cec4207a8244c7484f223
Kyle Richnafsky <7krich@gmail.com>
2022-04-24 12:39:57 -0400

    server retrieves candidates table upon api/cadidates endpoint

--- server.js
+++ server.js
@@ -25,10 +25,24 @@ const db = mysql.createConnection(
 
 // query database to test connection by returning all data in candidates table
 // query method exectues callback with resulting rows matching the query
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     // display each row as an object
-//     console.log(rows);
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            // exit databse once error is encountered
+            return;
+        }
+
+        // display rows as an object in browser and tell user get request was sucess
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // get a single candidate
 // db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
@@ -47,16 +61,16 @@ const db = mysql.createConnection(
 // });
 
 // create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-    VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-    if (err) {
-     console.log(err);
-    }
-    console.log(result);
-});
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//      console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // default response for any other request not supported by the app (not found)
 // catch all for endpoints not supported
